a2098693d262cab524c1e7b084a26ed84fdc48a1
Robin Heitz <robin.heitz@rwth-aachen.de>
2024-02-26 07:44:41 +0100

    Removed null-ls & fixed Goto definition keymap

--- lua/robin/plugins/lsp/lspconfig.lua
+++ lua/robin/plugins/lsp/lspconfig.lua
@@ -24,12 +24,8 @@ local on_attach = function(client, bufnr)
 	local opts = { noremap = true, silent = true, buffer = bufnr }
 
 	-- set keybinds
-	keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
-
-	keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts) -- got to declaration
-	-- keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
-
-	keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
+	keymap.set("n", "gD", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
+	keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<CR>", opts) -- see definition and make edits in window
 	keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
 	keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
 	keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
